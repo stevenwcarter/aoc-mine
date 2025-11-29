f9c978559fe123da080f89daa2fdf83d7ab12535
Steven Carter <steve@javapl.us>
2025-11-28 23:07:43 -0500

    Adding gcd and lcm methods

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,12 @@ pub use grid::*;
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
+pub fn lcm(a: u64, b: u64) -> u64 {
+    a * b / gcd(a, b)
+}
+pub fn gcd(a: u64, b: u64) -> u64 {
+    if b == 0 { a } else { gcd(b, a % b) }
+}
 
 #[cfg(test)]
 mod tests {
@@ -16,5 +22,12 @@ mod tests {
     fn it_works() {
         let result = add(2, 2);
         assert_eq!(result, 4);
+    }
+    #[test]
+    fn lcm_gcd_test() {
+        let a = 12;
+        let b = 15;
+        assert_eq!(gcd(a, b), 3);
+        assert_eq!(lcm(a, b), 60);
     }
 }
